d34e0b22aeee7fdf9b06aa600c37f00a2619cb7e
Patrick <pambonian@gmail.com>
2022-03-06 16:45:29 -0500

    comment

--- src/components/About.js
+++ src/components/About.js
@@ -24,6 +24,7 @@ export default function About() {
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               See My Past Work
             </a> */}
+            {/* remember to add previous work component */}
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
